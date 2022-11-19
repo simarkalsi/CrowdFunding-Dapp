ad09e976ee98bbc4141ac14ea44af3f461183b58
simar kalsi <singhsimar280@gmail.com>
2022-11-19 20:15:50 +0530

    2nd commit

--- pages/[address].js
+++ pages/[address].js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import Image from "next/image";
 import {ethers} from 'ethers';
 import CampaignFactory from '../artifacts/contracts/Campaign.sol/CampaignFactory.json'
-import Campaign from '../artifacts/contracts/Campaign.sol/Campaign.json'
+import Campaign from '../artifacts/contracts/Campaign.sol/Campaign.json'  
 import { useEffect, useState } from "react";
 
 
